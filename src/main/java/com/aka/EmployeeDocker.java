f6914f55c6519f3bb3dacb29b90ca9e8ec6b8f52
arnaudkaninda <kanindaarnaud@gmail.com>
2024-03-22 07:36:52 +0100

    Setting Docker compilation and run

--- src/main/java/com/aka/EmployeeDocker.java
+++ src/main/java/com/aka/EmployeeDocker.java
@@ -2,9 +2,16 @@ package com.aka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmployeeDocker {
+public class EmployeeDocker extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EmployeeDocker.class);
+    }
     public static void main (String [] args) {
         SpringApplication.run(EmployeeDocker.class, args);
     }
