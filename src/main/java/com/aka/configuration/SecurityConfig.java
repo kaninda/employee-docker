307dfc1efa8e58b87a0e650b28ebca88c24024f5
arnaudkaninda <kanindaarnaud@gmail.com>
2024-03-25 07:20:05 +0100

    desactive security temporary

---
+++ src/main/java/com/aka/configuration/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.aka.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .anyRequest().permitAll())
+                .httpBasic(Customizer.withDefaults()); // Utilisation de Customizer pour configurer httpBasic
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user")
+                .password(encoder.encode("password"))
+                .roles("USER").build());
+        manager.createUser(User.withUsername("admin")
+                .password(encoder.encode("admin"))
+                .roles("USER", "ADMIN").build());
+        return manager;
+    }
+}
---
+++ src/test/java/com/aka/employeegestion/EmployeeGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aka.employeegestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeGestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
